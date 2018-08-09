70b5ffd9257b82b673e1eb647074370e9de6f667
Tyler Smith <tylersmith.me@gmail.com>
2018-08-08 23:09:02 -0700

    TESTS: Fix TestVersionForNewListing test cases.

--- core/listings_test.go
+++ core/listings_test.go
@@ -33,9 +33,9 @@ func TestVersionForNewListing(t *testing.T) {
 		{1, core.PriceModifierListingVersion, pb.Listing_Metadata_FIXED_PRICE, core.PriceModifierListingVersion - 1},
 
 		// Old version - wrong current version
-		{1, core.PriceModifierListingVersion - 1, pb.Listing_Metadata_FIXED_PRICE, core.PriceModifierListingVersion - 1},
-		{1, core.PriceModifierListingVersion + 1, pb.Listing_Metadata_FIXED_PRICE, core.PriceModifierListingVersion + 1},
-		{1, core.PriceModifierListingVersion + 7, pb.Listing_Metadata_FIXED_PRICE, core.PriceModifierListingVersion + 7},
+		{1, core.PriceModifierListingVersion - 1, pb.Listing_Metadata_MARKET_PRICE, core.PriceModifierListingVersion - 1},
+		{1, core.PriceModifierListingVersion + 1, pb.Listing_Metadata_MARKET_PRICE, core.PriceModifierListingVersion + 1},
+		{1, core.PriceModifierListingVersion + 7, pb.Listing_Metadata_MARKET_PRICE, core.PriceModifierListingVersion + 7},
 
 		// New version
 		{1, core.PriceModifierListingVersion, pb.Listing_Metadata_MARKET_PRICE, core.PriceModifierListingVersion},
